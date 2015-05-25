c51a8c8a5fb44f724917026fba30c26b6af8ca8d
amyng <amyng95132@yahoo.com>
2015-05-24 19:58:32 -0700

    add db port number

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <html> 
     <body> 
 <?php
-if ($_POST['submit']) {
+//if ($_POST['submit']) {
 $db = pg_connect("host=ec2-54-204-24-154.compute-1.amazonaws.com port=5432 dbname=dbjod4rptv3953 user=ltlvdznvovjwbi password=-ZqJ5BlJLigG94kv24HDCF7Cf4");
 
 $firstname = pg_escape_string($_POST['firstname']); 
--- contact.php
+++ contact.php
@@ -13,32 +13,28 @@
 <?php include 'navigation.php';?>
 
 <div id="content">
-        <h1>Contact Amy3:</h1>
+        <h1>Contact Amy4:</h1>
  
  <form action="add.php" method="post">
     <div>
-        <label for="firstName">First Name:</label>
-        <input type="text" id="firstname" size="40" length="40">
+        First Name:<input type="text" name="firstname" size="40" length="40">
     </div>
 
     <div>
-        <label for="lastName">Last Name:</label>
-        <input type="text" id="lastname" size="40" length="40">
+        Last Name:<input type="text" name="lastname" size="40" length="40">
     </div>
 
     <div>
-        <label for="email">Email:</label>
-        <input type="text" id="email" size="40" length="40">
+        Email:<input type="text" name="email" size="40" length="40">
     </div>
 
     <div>
-        <label for="comment">Comment:</label>
-        <textarea id="comment" size="600" length="600"></textarea>
+        Comment:<textarea name="comment" size="600" length="600"></textarea>
     </div>
 
 
     <div class="submitBtn">
-        <button type="submit">Send your message</button>
+        <button type="submit" name="submit">Send your message</button>
     </div>
 </form>
 </div>
