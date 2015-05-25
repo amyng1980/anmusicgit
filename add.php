7e712e779b52334034a95d6eb025604dbcf31ed5
amyng <amyng95132@yahoo.com>
2015-05-24 19:43:27 -0700

    add db port number

--- add.php
+++ add.php
@@ -2,7 +2,7 @@
     <body> 
 <?php
 if ($_POST['submit']) {
-$db = pg_connect('host=ec2-54-204-24-154.compute-1.amazonaws.com dbname=dbjod4rptv3953 user=ltlvdznvovjwbi password=-ZqJ5BlJLigG94kv24HDCF7Cf4');
+$db = pg_connect("host=ec2-54-204-24-154.compute-1.amazonaws.com port=5432 dbname=dbjod4rptv3953 user=ltlvdznvovjwbi password=-ZqJ5BlJLigG94kv24HDCF7Cf4");
 
 $firstname = pg_escape_string($_POST['firstname']); 
 $lastname = pg_escape_string($_POST['lastname']); 
