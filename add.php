0be34f78447539894e06b9d55e90e5a9dcbc2468
amyng <amyng95132@yahoo.com>
2015-06-03 09:52:15 -0700

    update submit button to push the data to db

--- add.php
+++ add.php
@@ -1,36 +1,36 @@
 <html> 
     <body> 
-<?php
-if ($_POST['submit']) {
-$db = pg_connect("host=ec2-54-204-24-154.compute-1.amazonaws.com port=5432 dbname=dbjod4rptv3953 user=ltlvdznvovjwbi password=-ZqJ5BlJLigG94kv24HDCF7Cf4");
+<!--<?php
+// if ($_POST['submit']) {
+// $db = pg_connect("host=ec2-54-204-24-154.compute-1.amazonaws.com port=5432 dbname=dbjod4rptv3953 user=ltlvdznvovjwbi password=-ZqJ5BlJLigG94kv24HDCF7Cf4");
 
-$firstname = pg_escape_string($_POST['firstname']); 
-$lastname = pg_escape_string($_POST['lastname']); 
-$email = pg_escape_string($_POST['email']); 
-$comment = pg_escape_string($_POST['comment']); 
+// $firstname = pg_escape_string($_POST['firstname']); 
+// $lastname = pg_escape_string($_POST['lastname']); 
+// $email = pg_escape_string($_POST['email']); 
+// $comment = pg_escape_string($_POST['comment']); 
 
-$query = "INSERT INTO webform(firstname, lastname, email, comment) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $comment . "')";
+// $query = "INSERT INTO webform(firstname, lastname, email, comment) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $comment . "')";
 
-$result = pg_query($db, $query);
+// $result = pg_query($db, $query);
 
- if (!$result) {
-  $errormessage = pg_last_error(); 
-    echo "Error with query: " . $errormessage; 
-    exit(); 
-  } 
+ // if (!$result) {
+ //  $errormessage = pg_last_error(); 
+ //    echo "Error with query: " . $errormessage; 
+ //    exit(); 
+ //  } 
 
-printf ("These values were inserted into the database:");
+// printf ("These values were inserted into the database:");
 
-printf ("First name: %s <br />", $firstname); 
-printf ("Last name: %s <br />", $lastname); 
-printf ("Email address: %s <br />", $email); 
-printf ("Comment: %s", $comment); 
+// printf ("First name: %s <br />", $firstname); 
+// printf ("Last name: %s <br />", $lastname); 
+// printf ("Email address: %s <br />", $email); 
+// printf ("Comment: %s", $comment); 
 
 //var_dump($result);
-pg_close();
+//pg_close();
 //$result->closeCursor();
 }
-?>
-Click <a href="index.php">here</a> to go back to the home page.</p>
+?>-->
+Thank you for your submission. Click <a href="index.php">here</a> to go back to the home page.</p>
 </body>
 </html>
--- contact.php
+++ contact.php
@@ -36,12 +36,33 @@ if(!empty($error)){
          echo "$err <br />";
     }
 }else{
-        header('Location: /add.php');   
+
+$db = pg_connect("host=ec2-54-204-24-154.compute-1.amazonaws.com port=5432 dbname=dbjod4rptv3953 user=ltlvdznvovjwbi password=-ZqJ5BlJLigG94kv24HDCF7Cf4");
+
+$firstname = pg_escape_string($_POST['firstname']); 
+$lastname = pg_escape_string($_POST['lastname']); 
+$email = pg_escape_string($_POST['email']); 
+$comment = pg_escape_string($_POST['comment']); 
+
+$query = "INSERT INTO webform(firstname, lastname, email, comment) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $comment . "')";
+
+$result = pg_query($db, $query);
+
+    if (!$result) {
+        $errormessage = pg_last_error(); 
+        echo "Error with query: " . $errormessage; 
+        exit(); 
+    } 
+
+pg_close();
+header('Location: /add.php');   
 }
+
+
 }
 ?>
 <div id="content">
-<h1>Contact Amy 2:</h1>
+<h1>Contact Amy 3:</h1>
  
  <form action="<?php echo htmlentities( $_SERVER['PHP_SELF'] );?>" method="post">
 
